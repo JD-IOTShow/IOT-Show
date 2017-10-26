be404f74e840e4df6c7e9d3fc3546b31b0f2e14f
Bruce <brucehu@qq.com>
2017-10-26 19:15:36 +0800

    init

--- src/main/java/com/jdnw/iotshow/controller/ShowController.java
+++ src/main/java/com/jdnw/iotshow/controller/ShowController.java
@@ -1,8 +1,15 @@
 package com.jdnw.iotshow.controller;
 
+import ch.qos.logback.core.joran.spi.XMLUtil;
+import com.jdnw.iotshow.util.SoapClient;
+import com.jdnw.iotshow.util.XmlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * AUTHOR: zxy
@@ -21,5 +28,26 @@ public class ShowController {
 
     @RequestMapping("/send")
     public void sendMessage(){
+    }
+
+    @RequestMapping("/commonAbilityCallCnt")
+    @ResponseBody
+    public String commonAbilityCallCnt(){
+        SoapClient soapClient = new SoapClient("commonAbilityCallCnt",
+                "http://10.3.6.40:9773/services/dw_admin?wsdl");
+        Map<String, String> patameterMap = new HashMap<String, String>(2);
+        patameterMap.put("VC_USER_ID", "ALL");
+        patameterMap.put("TENANT_ID", "ALL");
+
+        String soapRequestData = soapClient.buildRequestData(patameterMap);
+        System.out.println(soapRequestData);
+        String result = null;
+        try {
+            String soapResponseData = soapClient.invoke(patameterMap);
+            result = XmlUtil.xml2JSON(soapResponseData.getBytes()).toJSONString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
