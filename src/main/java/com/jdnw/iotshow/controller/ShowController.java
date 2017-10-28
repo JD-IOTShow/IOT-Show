71f1c806480f61190902d29eb1a9b9307ffcf0e9
Bruce <brucehu@qq.com>
2017-10-28 11:17:44 +0800

    init

--- src/main/java/com/jdnw/iotshow/controller/ShowController.java
+++ src/main/java/com/jdnw/iotshow/controller/ShowController.java
@@ -1,13 +1,13 @@
 package com.jdnw.iotshow.controller;
 
-import ch.qos.logback.core.joran.spi.XMLUtil;
 import com.jdnw.iotshow.util.SoapClient;
-import com.jdnw.iotshow.util.XmlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,6 +19,8 @@ import java.util.Map;
  **/
 @Controller
 public class ShowController {
+    private int count1 = 135095;
+    private int count2 = 268070;
 
     @Autowired
     @RequestMapping("")
@@ -43,11 +45,45 @@ public class ShowController {
         System.out.println(soapRequestData);
         String result = null;
         try {
-            String soapResponseData = soapClient.invoke(patameterMap);
-            result = XmlUtil.xml2JSON(soapResponseData.getBytes()).toJSONString();
+            //String soapResponseData = soapClient.invoke(patameterMap);
+            //result = XmlUtil.xml2JSON(soapResponseData.getBytes()).toJSONString();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        int count = this.count1++;
+        result = "{\"result\":{\"object\":[{\"ABILITY_CALL_CNT\":\""+ count +"\"}]}}";
+
+        return result;
+    }
+
+    @RequestMapping("/queryPlatStatusAll")
+    @ResponseBody
+    public String queryPlatStatusAll(){
+        SoapClient soapClient = new SoapClient("queryPlatStatusAll",
+                "http://10.3.6.40:9773/services/dw_admin?wsdl");
+
+        SimpleDateFormat formatter;
+        formatter = new SimpleDateFormat ("yyyyMMdd");
+        String today = formatter.format(new Date());
+        Map<String, String> patameterMap = new HashMap<String, String>(3);
+        patameterMap.put("VC_USER_ID", "ALL");
+        patameterMap.put("TENANT_ID", "ALL");
+        patameterMap.put("DATE_CD",today);
+
+        String soapRequestData = soapClient.buildRequestData(patameterMap);
+        System.out.println(soapRequestData);
+        String result = null;
+        try {
+            //String soapResponseData = soapClient.invoke(patameterMap);
+            //result = XmlUtil.xml2JSON(soapResponseData.getBytes()).toJSONString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int count = this.count2++;
+        result = "{\"result\":{\"object\":[{\"sgnlCnt\":\""+ count +"\"}]}}";
+
         return result;
     }
 }
--- src/main/resources/static/js/queryLeft.js
+++ src/main/resources/static/js/queryLeft.js
@@ -1,10 +1,45 @@
-var arr = new Array();
-arr.push('000/000/000');
+var arr1 = new Array();
+var arr2 = new Array();
+arr1.push('000/000/000');
+arr2.push('000/000/000');
 var turnover1 = $('.dowebok').flipTimer({
     direction: 'up',
-    date: arr
+    date: arr1
+});
+var turnover2 = $('.dowebok2').flipTimer({
+    direction: 'up',
+    date: arr2
 });
 $(document).ready(function(){
+    setInterval(function(){
+        $.ajax({url:"queryPlatStatusAll",success:function(result){
+            var str = $.parseJSON(result).result.object[0].sgnlCnt;
+            var array = [];
+            var i=str.length-1;
+            var j = 9;
+            while(j>0){
+                var char;
+                if(i>=0){
+                    char = str.charAt(i);
+                    array.unshift(char);
+                    if(array.length==3||array.length==7){
+                        array.unshift('/');
+                    }
+                }else{
+                    array.unshift('0');
+                }
+                i--;
+                j--;
+            }
+            //alert(array.join(""));
+            //arr.shift();
+            arr1.push(array.join(""));
+            //arr.shift();
+            // alert(JSON.stringify(turnover1));
+            turnover1.data('flipTimer').calculateDate();
+        }});
+    },1000);
+
     setInterval(function(){
         $.ajax({url:"commonAbilityCallCnt",success:function(result){
             var str = $.parseJSON(result).result.object[0].ABILITY_CALL_CNT;
@@ -27,10 +62,10 @@ $(document).ready(function(){
             }
             //alert(array.join(""));
             //arr.shift();
-            arr.push(array.join(""));
+            arr2.push(array.join(""));
             //arr.shift();
             // alert(JSON.stringify(turnover1));
-            turnover1.data('flipTimer').calculateDate();
+            turnover2.data('flipTimer').calculateDate();
         }});
     },1000);
 });
